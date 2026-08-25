3f0a5d995d17ea086135ea330dc299f0d1299133
Igor Lima <igor.riopequeno@hotmail.com>
2026-08-25 17:10:03 -0300

    Added signing for APK to fix the apk updater 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.moondicine.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
